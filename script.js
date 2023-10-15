9f5fc94c31f53323212e13f9776488d45d19bc48
Nattapol_C <nattapolch.t@gmail.com>
2023-10-15 20:20:27 +0700

    Try Modal R1

--- script.js
+++ script.js
@@ -8,25 +8,36 @@ const refreshPriceItem_2 = function () {
   price_radio_2 = document.querySelector('input[name="price-cart"]:checked');
   pricePerItem_2.innerHTML = "$" + price_radio_2.value * qty_item_2.value;
 };
-const refreshTotalPrice = function(){
+const refreshTotalPrice = function () {
   let checkItem1 = document.querySelector(".cartItem1");
   let checkItem2 = document.querySelector(".cartItem2");
-  if(!checkItem1.classList.contains('d-none')){
-    priceItem1 = Number(price_radio.value ) * Number(input.value)
-  }
-  else{
+  if (!checkItem1.classList.contains("d-none")) {
+    priceItem1 = Number(price_radio.value) * Number(input.value);
+  } else {
     priceItem1 = 0;
   }
-  if(!checkItem2.classList.contains('d-none')){
-    priceItem2 = Number(price_radio_2.value ) * Number(qty_item_2.value)
-  }
-  else{
+  if (!checkItem2.classList.contains("d-none")) {
+    priceItem2 = Number(price_radio_2.value) * Number(qty_item_2.value);
+  } else {
     priceItem2 = 0;
   }
-  
-  sumPrice = priceItem1 + priceItem2
-  totalPrice.innerHTML = "$" + sumPrice 
-}
+
+  sumPrice = priceItem1 + priceItem2;
+  totalPrice.innerHTML = "$" + sumPrice;
+};
+const openModal = function () {
+  console.log("Opening modal");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const btnCloseModal = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
 
 const totalPrice = document.querySelector(".totalPrice");
 const btnRemove = document.querySelectorAll(".button-remove");
@@ -42,9 +53,17 @@ const pricePerItem = document.querySelector(".pricePerItem");
 const pricePerItem_2 = document.querySelector(".pricePerItem_2");
 let price_radio = document.querySelector('input[name="price-base"]:checked');
 let price_radio_2 = document.querySelector('input[name="price-cart"]:checked');
-let sum_quantity,priceItem1,priceItem2,sumPrice;
-refreshPriceItem();
-refreshTotalPrice();
+let sum_quantity, priceItem1, priceItem2, sumPrice;
+const btnOpenModal = document.querySelector(".openModal");
+btnOpenModal.addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
 btnplus.addEventListener("click", function () {
   sum_quantity = Number(input.value) + 1;
   input.value = sum_quantity;
@@ -87,3 +106,5 @@ btnRemove.forEach((button) => {
     refreshTotalPrice();
   });
 });
+refreshPriceItem();
+refreshTotalPrice();
